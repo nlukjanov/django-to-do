697fbc1a1b1bbcbfa24f24a3ed31c326a359494c
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-07-09 11:21:35 +0100

    add form

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-
+# pylint: disable=no-member
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/list.html', context)
