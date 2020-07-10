a58b816be77a8771052414e8c81a64032178f933
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-07-10 17:37:47 +0100

    add updated task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, update_task
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='list'),
+    path('update_task/<str:pk>/', update_task, name='update_task')
 ]
