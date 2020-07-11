cc1042617761ccef9737bea8606c9e19c2d9ed4c
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-07-11 09:25:52 +0100

    add delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, update_task
+from .views import index, update_task, delete_task
 
 urlpatterns = [
     path('', index, name='list'),
-    path('update_task/<str:pk>/', update_task, name='update_task')
+    path('update_task/<str:pk>/', update_task, name='update_task'),
+    path('delete_task/<str:pk>/', delete_task, name='delete_task')
 ]
